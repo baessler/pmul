9f15bf43a76dcdd777524edcd5f99d5fb0c96a9b
Andreas Baessler <a_baessler@web.de>
2017-10-15 21:36:26 +0200

    new version for pip

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pmul',
   packages = ['pmul'], # this must be the same as the name above
-  version = '0.3',
+  version = '0.4',
   description = 'An implementation of the P_MUL protocol in python',
   author = 'Andreas Baessler',
   author_email = 'a_baessler@web.de',
   url = 'https://github.com/baessler/pmul', # use the URL to the github repo
-  download_url = 'https://github.com/baessler/pmul/archive/0.3.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/baessler/pmul/archive/0.4.tar.gz', # I'll explain this in a second
   keywords = ['p_mul', 'pmul', 'transport', 'protocol', 'multicast', 'udp'], # arbitrary keywords
   classifiers = [],
 )
